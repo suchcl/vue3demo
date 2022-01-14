40ce570c5fcc510b4fb94047589aa710c2074a64
suchcl <ya5969@163.com>
2022-01-15 05:37:12 +0800

    demo

--- vue2_git_eslint_prettier/src/main.js
+++ vue2_git_eslint_prettier/src/main.js
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import ElementPlus from "element-plus";
+// import axios from "./assets/js/http";
+import 'element-plus/dist/index.css';
+import 'element-plus/theme-chalk/display.css';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(ElementPlus);
+app.mount("#app");
+// createApp(App).use(ElementPlus).mount('#app')
