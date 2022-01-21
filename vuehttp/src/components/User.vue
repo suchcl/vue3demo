1758e23ac08d5337d0e01786435c9a107bb4ef07
suchcl <ya5969@163.com>
2022-01-21 10:17:51 +0800

    vue中http链接测试

---
+++ vuehttp/src/components/User.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="user">
+        <h3>用户</h3>
+        <button @click="getUser">获取用户信息</button>
+        <ul>
+            <li v-for="itemm in userList" :key="itemm.id">{{ itemm.username }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import axios from "../assets/js/http";
+export default {
+    data() {
+        return {
+            msg: "hello",
+            userList: []
+        }
+    },
+    methods: {
+        getUser() {
+            axios.get("/getUser").then((res) => {
+                const { userInfo } = res.data.data;
+                this.userList = userInfo;
+            });
+        }
+    }
+}
+</script>
