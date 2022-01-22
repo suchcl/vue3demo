013ee59c2bb6bea84aea78ec9cc0caa4c9df73c0
suchcl <ya5969@163.com>
2022-01-22 09:15:36 +0800

    less配置

--- vuedemo/src/components/HelloWorld.vue
+++ vuedemo/src/components/HelloWorld.vue
@@ -2,6 +2,10 @@
   <div class="hello">
     <p>msg</p>
     <p class="text">测试下样式</p>
+    <ul>
+      <li>苹果</li>
+      <li>香蕉</li>
+    </ul>
   </div>
 </template>
 <script>
@@ -16,6 +20,6 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="less" scoped>
 .text {
-  color: #f20;
+  color: #369;
 }
 </style>
--- vuedemo/src/main.js
+++ vuedemo/src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import "./assets/less/base.less"
 import App from './App.vue'
 
 Vue.config.productionTip = false
